5551758e788a88a2dae1619c0280f888f31dcabb
RyanStoffel <772612@calbaptist.edu>
2025-10-08 11:35:47 -0700

    Refactored Main.java to use Java 25 standards.

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,28 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("os.name"));
+void main(){
+    System.out.println(System.getProperty("os.name"));
 
-        File sourceCode = new File("stof/main.stof");
-        StringBuilder fileString = new StringBuilder();
+    File sourceCode = new File("stof/main.stof");
+    StringBuilder fileString = new StringBuilder();
 
-        try (Scanner reader = new Scanner(sourceCode)) {
-            while (reader.hasNextLine()) {
-                String data = reader.nextLine();
-                fileString.append(data).append("\n"); // preserve newlines
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("An error has occurred: " + e.getMessage());
-            return;
+    try (Scanner reader = new Scanner(sourceCode)) {
+        while (reader.hasNextLine()) {
+            String data = reader.nextLine();
+            fileString.append(data).append("\n"); // preserve newlines
         }
-
-        new Tokenizer(fileString.toString());
-
-        Tokenizer tokenizer = new Tokenizer(fileString.toString());
-        Vector<Tokenizer.Token> tokens = tokenizer.tokenize();
-        for (Tokenizer.Token token : tokens) {
-            System.out.print(token + " ");
-        }
-        System.out.println();
+    } catch (FileNotFoundException e) {
+        System.out.println("An error has occurred: " + e.getMessage());
+        return;
     }
+
+    new Tokenizer(fileString.toString());
+
+    Tokenizer tokenizer = new Tokenizer(fileString.toString());
+    Vector<Tokenizer.Token> tokens = tokenizer.tokenize();
+    for (Tokenizer.Token token : tokens) {
+        System.out.print(token + " ");
+    }
+    System.out.println();
 }
