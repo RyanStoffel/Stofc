8f97c8a55d52d3f90d88bc00724e9ff8b1756d1d
RyanStoffel <772612@calbaptist.edu>
2025-10-07 19:44:14 -0700

    Organized keywords to easily identify them.

--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -5,39 +5,54 @@ public class Tokenizer {
     /// UTILITY CLASSES ///
     ///////////////////////
     public enum TokenType {
-        STOF_EXIT,
-        STOF_INT_LITERAL,
-        STOF_SEMICOLON,
-        STOF_OPEN_PARENTHESIS,
-        STOF_CLOSE_PARENTHESIS,
-        STOF_IDENTIFIER,
+        // variable types
+        STOF_INT,
+        STOF_VAR,
         STOF_LET,
-        STOF_EQUALS,
+        STOF_BOOLEAN,
+
+        // exit and return statements
+        STOF_EXIT,
+        STOF_RETURN,
+
+        // operators
         STOF_PLUS,
         STOF_MINUS,
         STOF_MULTIPLY,
         STOF_DIVIDE,
-        STOF_OPEN_CURLY_BRACKET,
-        STOF_CLOSE_CURLY_BRACKET,
-        STOF_IF,
-        STOF_ELIF,
-        STOF_ELSE,
-        STOF_OPEN_SQUARE_BRACKET,
-        STOF_CLOSE_SQUARE_BRACKET,
-        STOF_RETURN,
         STOF_MODULO,
-        STOF_FOR,
-        STOF_WHILE,
-        STOF_SWITCH,
-        STOF_CASE,
-        STOF_VAR,
-        STOF_INT,
-        STOF_CLASS,
-        STOF_BOOLEAN,
+        STOF_EQUALS,
         STOF_GREATER_THAN,
         STOF_LESS_THAN,
         STOF_GREATER_THAN_OR_EQUAL,
         STOF_LESS_THAN_OR_EQUAL,
+
+        // basic syntax
+        STOF_OPEN_PARENTHESIS,
+        STOF_CLOSE_PARENTHESIS,
+        STOF_OPEN_CURLY_BRACKET,
+        STOF_CLOSE_CURLY_BRACKET,
+        STOF_OPEN_SQUARE_BRACKET,
+        STOF_CLOSE_SQUARE_BRACKET,
+        STOF_SEMICOLON,
+
+        // class
+        STOF_CLASS,
+
+        // conditional statements
+        STOF_IF,
+        STOF_ELIF,
+        STOF_ELSE,
+        STOF_SWITCH,
+        STOF_CASE,
+
+        // loops
+        STOF_FOR,
+        STOF_WHILE,
+
+        // numbers and variable names
+        STOF_INT_LITERAL,
+        STOF_IDENTIFIER,
     }
 
     public static class Token {
@@ -58,39 +73,54 @@ public class Tokenizer {
         @Override
         public String toString() {
             return switch (type) {
-                case STOF_EXIT -> "exit";
-                case STOF_INT_LITERAL -> value;
-                case STOF_SEMICOLON -> ";";
-                case STOF_OPEN_PARENTHESIS -> "(";
-                case STOF_CLOSE_PARENTHESIS -> ")";
-                case STOF_IDENTIFIER -> "identifier";
+                // variable types
+                case STOF_INT -> "int";
+                case STOF_VAR -> "var";
                 case STOF_LET -> "let";
-                case STOF_EQUALS -> "=";
+                case STOF_BOOLEAN -> "boolean";
+
+                // exit and return statements
+                case STOF_EXIT -> "exit";
+                case STOF_RETURN -> "return";
+
+                // operators
                 case STOF_PLUS -> "+";
                 case STOF_MINUS -> "-";
                 case STOF_MULTIPLY -> "*";
                 case STOF_DIVIDE -> "/";
-                case STOF_OPEN_CURLY_BRACKET -> "{";
-                case STOF_CLOSE_CURLY_BRACKET -> "}";
-                case STOF_IF -> "if";
-                case STOF_ELIF -> "elif";
-                case STOF_ELSE -> "else";
-                case STOF_OPEN_SQUARE_BRACKET -> "[";
-                case STOF_CLOSE_SQUARE_BRACKET -> "]";
-                case STOF_RETURN -> "return";
                 case STOF_MODULO -> "%";
-                case STOF_FOR -> "for";
-                case STOF_WHILE -> "while";
-                case STOF_SWITCH -> "switch";
-                case STOF_CASE -> "case";
-                case STOF_VAR -> "var";
-                case STOF_INT -> "int";
-                case STOF_CLASS -> "class";
-                case STOF_BOOLEAN -> "boolean";
+                case STOF_EQUALS -> "=";
                 case STOF_GREATER_THAN -> ">";
                 case STOF_LESS_THAN -> "<";
                 case STOF_GREATER_THAN_OR_EQUAL -> ">=";
                 case STOF_LESS_THAN_OR_EQUAL -> "<=";
+
+                // basic syntax
+                case STOF_OPEN_PARENTHESIS -> "(";
+                case STOF_CLOSE_PARENTHESIS -> ")";
+                case STOF_OPEN_CURLY_BRACKET -> "{";
+                case STOF_CLOSE_CURLY_BRACKET -> "}";
+                case STOF_OPEN_SQUARE_BRACKET -> "[";
+                case STOF_CLOSE_SQUARE_BRACKET -> "]";
+                case STOF_SEMICOLON -> ";";
+
+                // class
+                case STOF_CLASS -> "class";
+
+                // conditional statements
+                case STOF_IF -> "if";
+                case STOF_ELIF -> "elif";
+                case STOF_ELSE -> "else";
+                case STOF_SWITCH -> "switch";
+                case STOF_CASE -> "case";
+
+                // loops
+                case STOF_FOR -> "for";
+                case STOF_WHILE -> "while";
+
+                // numbers and variable names
+                case STOF_INT_LITERAL -> value;
+                case STOF_IDENTIFIER -> "identifier";
             };
         }
     }
@@ -153,20 +183,27 @@ public class Tokenizer {
             }
 
             switch (c) {
+                // basic syntax
                 case '(' -> tokens.add(new Token(TokenType.STOF_OPEN_PARENTHESIS));
                 case ')' -> tokens.add(new Token(TokenType.STOF_CLOSE_PARENTHESIS));
-                case '[' -> tokens.add(new Token(TokenType.STOF_OPEN_SQUARE_BRACKET));
-                case ']' -> tokens.add(new Token(TokenType.STOF_CLOSE_SQUARE_BRACKET));
                 case '{' -> tokens.add(new Token(TokenType.STOF_OPEN_CURLY_BRACKET));
                 case '}' -> tokens.add(new Token(TokenType.STOF_CLOSE_CURLY_BRACKET));
-                case '='  -> tokens.add(new Token(TokenType.STOF_EQUALS));
-                case '+'  -> tokens.add(new Token(TokenType.STOF_PLUS));
-                case '-'  -> tokens.add(new Token(TokenType.STOF_MINUS));
-                case '*'  -> tokens.add(new Token(TokenType.STOF_MULTIPLY));
-                case '/'  -> tokens.add(new Token(TokenType.STOF_DIVIDE));
-                case '<'  -> tokens.add(new Token(TokenType.STOF_GREATER_THAN));
-                case '>'  -> tokens.add(new Token(TokenType.STOF_LESS_THAN));
+                case '[' -> tokens.add(new Token(TokenType.STOF_OPEN_SQUARE_BRACKET));
+                case ']' -> tokens.add(new Token(TokenType.STOF_CLOSE_SQUARE_BRACKET));
                 case ';' -> tokens.add(new Token(TokenType.STOF_SEMICOLON));
+
+                // operators
+                case '=' -> tokens.add(new Token(TokenType.STOF_EQUALS));
+                case '+' -> tokens.add(new Token(TokenType.STOF_PLUS));
+                case '-' -> tokens.add(new Token(TokenType.STOF_MINUS));
+                case '*' -> tokens.add(new Token(TokenType.STOF_MULTIPLY));
+                case '/' -> tokens.add(new Token(TokenType.STOF_DIVIDE));
+                case '%' -> tokens.add(new Token(TokenType.STOF_MODULO));
+                case '<' -> tokens.add(new Token(TokenType.STOF_LESS_THAN));
+                case '>' -> tokens.add(new Token(TokenType.STOF_GREATER_THAN));
+
+                // default
+                default -> { /* handle unexpected characters later if needed */ }
             }
 
             if (Character.isDigit(c)) { // executes if the first character is NOT a letter, checks if it is a digit
